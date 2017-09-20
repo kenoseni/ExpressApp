6e148274ae2686d78c215a88b50eca9f9fc90fca
Olusola Oseni <kenolusola@gmail.com>
2017-09-20 16:09:21 +0100

    Puppy data included

---
+++ puppies.js
@@ -0,0 +1,26 @@
+module.exports = [
+    {
+        name: "Bush",
+        age: "6 days",
+        color: "black",
+        favFood: "milk"
+    },
+    {
+        name: "Lugzy",
+        age: "10 days",
+        color: "brown",
+        favFood: "meat"
+    },
+    {
+        name: "Bugzy",
+        age: "20 days",
+        color: "crimson",
+        favFood: "mutton"
+    },
+    {
+        name: "fluxy",
+        age: "1 day",
+        color: "white",
+        favFood: "milk"
+    }
+]
